73948ccb773547e25188ac085b8e3f2037fdd65b
popaula937 <paula.popa@codemart.ro>
2022-03-30 14:11:25 +0300

    WID-55. Add unit tests.

---
+++ tvbwidgets/tests/test_threed_widget.py
@@ -0,0 +1,60 @@
+import logging
+import numpy
+
+from tvb.datatypes.connectivity import Connectivity
+from tvb.datatypes.region_mapping import RegionMapping
+from tvb.datatypes.sensors import SensorsInternal
+from tvb.datatypes.surfaces import FaceSurface, CorticalSurface
+
+from tvbwidgets.ui.threed_widget import ThreeDWidget, Config
+
+
+def test_add_datatype(caplog, mocker):
+    widget = ThreeDWidget()
+
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        widget.add_datatype(None)
+        assert caplog.records[0].levelname == 'INFO'
+        assert 'None' in caplog.text
+
+    caplog.clear()
+    widget.add_datatype(RegionMapping())
+    assert caplog.records[0].levelname == 'INFO'
+    assert 'cmap' in caplog.text
+
+    caplog.clear()
+    widget.add_datatype(10)
+    assert caplog.records[0].levelname == 'INFO'
+    assert 'not supported' in caplog.text
+
+    def mock_update_plot(self):
+        """Mock plot drawing"""
+        pass
+
+    mocker.patch('tvbwidgets.ui.threed_widget.CustomOutput.update_plot', mock_update_plot)
+
+    connectivity = Connectivity(centres=numpy.zeros((10, 3)))
+    widget.add_datatype(connectivity)
+    assert widget.output_plot.total_actors == 1
+    assert len(widget.plot_controls.children) == 1
+
+    face = FaceSurface(vertices=numpy.zeros((10, 3)), triangles=numpy.zeros((10, 3), dtype=int))
+    widget.add_datatype(face)
+    assert widget.output_plot.total_actors == 2
+    assert len(widget.plot_controls.children) == 2
+    assert len(widget.surface_display_controls.children) == 1
+
+    seeg = SensorsInternal(locations=numpy.zeros((10, 3)))
+    widget.add_datatype(seeg)
+    assert widget.output_plot.total_actors == 3
+    assert len(widget.plot_controls.children) == 3
+
+    cortex = CorticalSurface(vertices=numpy.zeros((10, 3)), triangles=numpy.zeros((10, 3), dtype=int))
+    reg_map = RegionMapping(array_data=numpy.zeros(10, dtype=int))
+    config = Config(name='Cortex')
+    config.add_region_mapping_as_cmap(reg_map)
+    widget.add_datatype(cortex)
+    assert widget.output_plot.total_actors == 4
+    assert len(widget.plot_controls.children) == 4
+    assert len(widget.surface_display_controls.children) == 2
