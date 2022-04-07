23cb9c5b7b6f5c7c3dc6c213b0e44b6d295ef3e3
popaula937 <paula.popa@codemart.ro>
2022-04-07 15:51:22 +0300

    WID-58. Keep version in package.json.

--- setup.py
+++ setup.py
@@ -5,33 +5,41 @@
 # (c) 2022-2023, TVB Widgets Team
 #
 
-import os
-import shutil
+import json
 import setuptools
-
-TEAM = "Juelich SDL Neuroscience, INS - Marseille, Codemart"
+import shutil
+from pathlib import Path
 
 REQUIRED_PACKAGES = ["colorcet", "ebrains_drive", "ipywidgets", "pythreejs", "pyvista", "tvb-library"]
 
 REQUIRED_EXTRA_EXAMPLES = ["tvb-data"]
 REQUIRED_EXTRA_TESTS = ["pytest", "pytest-mock"]
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fd:
-    DESCRIPTION = fd.read()
+HERE = Path(__file__).parent.resolve()
+
+DESCRIPTION = (HERE / "README.md").read_text()
+
+# Get the package info from package.json
+pkg_json = json.loads((HERE / "package.json").read_bytes())
+version = (
+    pkg_json["version"]
+        .replace("-alpha.", "a")
+        .replace("-beta.", "b")
+        .replace("-rc.", "rc")
+)
 
 setuptools.setup(name='tvb-widgets',
+                 version=version,
                  packages=setuptools.find_packages(),
-                 use_scm_version=True,
-                 setup_requires=['setuptools_scm'],
                  include_package_data=True,
                  install_requires=REQUIRED_PACKAGES,
                  extras_require={"tests": REQUIRED_EXTRA_TESTS, "examples": REQUIRED_EXTRA_EXAMPLES},
-                 description='GUI widgets for EBRAINS showcases',
+                 description=pkg_json['description'],
                  long_description=DESCRIPTION,
-                 license="GPL-3.0-or-later",
-                 author=TEAM,
-                 author_email='tvb.admin@thevirtualbrain.org',
-                 url='https://github.com/the-virtual-brain/tvb-widgets',
+                 license=pkg_json['license'],
+                 author=pkg_json["author"]["name"],
+                 author_email=pkg_json["author"]["email"],
+                 url=pkg_json["homepage"],
                  keywords='tvb widgets jupyterlab ebrains showcases')
 
 # Cleanup after EGG install. These are created by running setup.py in the source tree
