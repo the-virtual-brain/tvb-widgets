c1b8a7d10d17cd262bf6f7a2a7a1c1833e6fb25d
915-Misan-Teodora <teodora.misan@yahoo.com>
2023-02-16 09:53:42 +0200

    WID-171: add change color features for PSE widget

--- tvbwidgets/ui/pse_widget.py
+++ tvbwidgets/ui/pse_widget.py
@@ -2,8 +2,6 @@ import plotly.graph_objects as go
 import numpy as np
 import ipywidgets as widgets
 from IPython.core.display_functions import display
-from IPython.core.display_functions import clear_output
-import plotly.offline as ofl
 
 
 class PSEWidget:
@@ -13,7 +11,9 @@ class PSEWidget:
         self.data = None
         self.layout = None
         self.figure = None
-        self.smoothFunct = False
+        self.smooth_effect = False
+        self.smooth_effect_checkbox = None
+        self.change_color_dropdown = None
         self._create_visualizer()
         self._populate_features()
 
@@ -30,28 +30,51 @@ class PSEWidget:
                                     r=50,
                                     b=100,
                                     t=100,
-                                    pad=4), title="Visualizer PSE", titlefont=dict(size=20,
+                                    pad=4), title="PSE Visualizer", titlefont=dict(size=20,
                                                                                    family='Arial, sans-serif'),
                                 )
         self.figure = go.FigureWidget(layout=self.layout)
         self._populate_visualizer()
-        self.figure.add_trace(go.Heatmap(z=self.data, showscale=True))
+        self.figure.add_trace(go.Heatmap(z=self.data, colorscale='RdBu', showscale=True))
 
     def _populate_features(self):
-        smooth_viz = widgets.Checkbox(False, description='Smooth visualizer')
-        smooth_viz.observe(self._change_smooth_effect, names="value", type="change")
-        features_area = widgets.Accordion(children=[smooth_viz], selected_index=None,
+        self._smooth_effect()
+        self._color_changed()
+        features = widgets.VBox(children=[self.smooth_effect_checkbox, self.change_color_dropdown])
+        features_area = widgets.Accordion(children=[features], selected_index=None,
                                           layout=widgets.Layout(width='25%', marginTop='100px'))
         features_area.set_title(0, 'Features')
-        table = widgets.HBox([self.figure, features_area])
+        table = widgets.HBox([features_area, self.figure])
         display(table)
 
-    def _change_smooth_effect(self, value):
-        if value['new']:
-            self.smoothFunct = 'best'
-        else:
-            self.smoothFunct = False
-        self.redraw()
+    def _smooth_effect(self):
+        self.smooth_effect_checkbox = widgets.Checkbox(False, description='Smooth visualizer',
+                                                       layout=widgets.Layout(margin='10px 0px 10px 0px'))
 
-    def redraw(self):
-        self.figure.update_traces(go.Heatmap(z=self.data, showscale=True, zsmooth=self.smoothFunct))
+        def smooth_effect_changed(change):
+            if change['type'] != 'change' or change['name'] != 'value':
+                return
+
+            if change['new']:
+                self.smooth_effect = 'best'
+            else:
+                self.smooth_effect = False
+            self.figure.update_traces(go.Heatmap(z=self.data, showscale=True, zsmooth=self.smooth_effect))
+
+        self.smooth_effect_checkbox.observe(smooth_effect_changed)
+
+    def _color_changed(self):
+        self.change_color_dropdown = widgets.Dropdown(
+            options=['Blackbody', 'Cividis', 'Earth', 'RdBu', 'Rainbow', 'Viridis', 'YlGnBu', 'YlOrRd'],
+            description="Color:",
+            value='RdBu',
+            disabled=False)
+
+        def change_color(change):
+            if change['type'] != 'change' or change['name'] != 'value':
+                return
+
+            self.figure.update_traces(go.Heatmap(z=self.data, showscale=True, colorscale=change['new']))
+
+        self.change_color_dropdown.observe(change_color)
+
