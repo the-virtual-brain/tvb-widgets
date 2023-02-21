accfd465510afcd101455808d76d247d97d032e6
915-Misan-Teodora <teodora.misan@yahoo.com>
2023-02-21 13:17:55 +0200

    WID-171: add plotly library where needed

--- dev/prepare_env.sh
+++ dev/prepare_env.sh
@@ -4,7 +4,7 @@
 conda update -n base -c defaults conda
 conda create -y --name tvb-widgets python=3.8 nomkl numba scipy numpy
 conda install -y --name tvb-widgets -c conda-forge jupyterlab tvb-library tvb-data ipywidgets==7.7.2
-conda install -y --name tvb-widgets -c conda-forge pythreejs pyvista colorcet
+conda install -y --name tvb-widgets -c conda-forge pythreejs pyvista colorcet plotly
 conda install -y --name tvb-widgets ipywidgets
 
 pip install --upgrade pip
