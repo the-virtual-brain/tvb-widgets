8803925326b56623a6ab0619071c3b4e228b7e53
rares.horge <rares.horge@codemart.ro>
2023-03-23 12:15:39 +0200

    WID-196: run notebooks in parallel

--- dev/notebook_runner.py
+++ dev/notebook_runner.py
@@ -7,40 +7,38 @@
 
 import os
 import sys
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
 
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
-def execute_notebook(in_path, out_path, notebook):
-    with open(os.path.join(in_path, notebook)) as f:
+def execute_notebook(in_path, notebook):
+    with open(os.path.join(in_path, notebook), encoding='utf-8') as f:
         nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=1000)
 
     ep.preprocess(nb)
 
-    with open(os.path.join(out_path, notebook), 'w+', encoding='utf-8') as f:
-        nbformat.write(nb, f)
+    # with open(os.path.join(out_path, notebook), 'w+', encoding='utf-8') as f: # for debug only
+    #     nbformat.write(nb, f)
     print(notebook + " - successful execution")
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
         in_path = sys.argv[1]
-        out_path = sys.argv[2]
     else:
-        print("please insert the input and output paths")
+        print("please insert the input path")
         exit(-1)
 
     if not os.getenv('CLB_AUTH'):
         os.environ['CLB_AUTH'] = 'abc'
 
-    if not os.path.exists(out_path):
-        os.mkdir(out_path)
-
     notebooks = [file for file in os.listdir(in_path) if file[-6:] == ".ipynb"]
 
-    for notebook in notebooks:
-        execute_notebook(in_path, out_path, notebook)
-
+    # start as many threads as logical cpus
+    with ThreadPool(cpu_count()) as pool:
+        pool.map(lambda notebook: execute_notebook(in_path, notebook), notebooks)
