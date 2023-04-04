68b0776cd889877611935282bee904da431e79ea
rares.horge <rares.horge@codemart.ro>
2023-04-04 21:32:18 +0300

    WID-191: run current command only if the previous one succeded in .bat file

--- dev/prepare_env.bat
+++ dev/prepare_env.bat
@@ -1,9 +1,9 @@
 rem Last tested with Python 3.8
 
 rem Anaconda needs to be installed upfront
-conda update -n base -c defaults conda & ^
-conda create -y --name tvb-widgets python=3.8 nomkl numba scipy numpy jupyterlab & ^
-conda install -y --name tvb-widgets -c conda-forge pyvista & ^
+conda update -n base -c defaults conda && ^
+conda create -y --name tvb-widgets python=3.8 nomkl numba scipy numpy jupyterlab && ^
+conda install -y --name tvb-widgets -c conda-forge pyvista && ^
 conda install -y --name tvb-widgets --file ..\requirements.txt
 
 rem Optionally install tvb-data from ZENODO to use demo data within it
