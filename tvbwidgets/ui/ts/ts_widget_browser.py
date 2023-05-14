0c35f030a05f89946cdadd99a26eb485a178df43
liadomide <lia_domide@yahoo.com>
2023-05-14 16:26:13 +0300

    WID-222 Add extra button in TimeSeriesBrowser to allow the user to immediately switch between plotly and mne backend

--- tvbwidgets/ui/ts/ts_widget_browser.py
+++ tvbwidgets/ui/ts/ts_widget_browser.py
@@ -8,6 +8,7 @@
 import ipywidgets as widgets
 from tvb.datatypes.time_series import TimeSeries
 from tvbwidgets.ui.ts.mne_ts_widget import TimeSeriesWidgetMNE
+from tvbwidgets.ui.ts.plotly_ts_widget import TimeSeriesWidgetPlotly
 from tvbwidgets.ui.widget_with_browser import TVBWidgetWithBrowser
 
 
@@ -15,16 +16,24 @@ class TimeSeriesBrowser(widgets.VBox, TVBWidgetWithBrowser):
 
     def __init__(self, collab=None, folder=None):
         super().__init__(**{'collab': collab, 'folder': folder})
-        timeseries_button = widgets.Button(description='View time series')
-        self.buttons = widgets.HBox([timeseries_button], layout=widgets.Layout(margin="0px 0px 0px 20px"))
-        self.timeseries_widget = TimeSeriesWidgetMNE()
-        self.children = [self.storage_widget, self.buttons, self.message_label, self.timeseries_widget]
+        btn_mne = widgets.Button(description='View TS with MNE')
+        btn_plotly = widgets.Button(description='View TS with Plotly')
+        self.buttons = widgets.HBox([btn_mne, btn_plotly], layout=widgets.Layout(margin="0px 0px 0px 20px"))
+        self.children = [self.storage_widget, self.buttons, self.message_label]
 
-        def add_timeseries_datatype(_):
+        def load_ts_for_mne(_):
+            self.timeseries_widget = TimeSeriesWidgetMNE()
+            self.children = [self.storage_widget, self.buttons, self.message_label, self.timeseries_widget]
             self.load_selected_file(TimeSeries, ('.h5', '.npz'))
 
-        timeseries_button.on_click(add_timeseries_datatype)
+        def load_ts_for_plotly(_):
+            self.timeseries_widget = TimeSeriesWidgetPlotly()
+            self.children = [self.storage_widget, self.buttons, self.message_label, self.timeseries_widget]
+            self.load_selected_file(TimeSeries, ('.h5', '.npz'))
+
+        btn_mne.on_click(load_ts_for_mne)
+        btn_plotly.on_click(load_ts_for_plotly)
 
     def add_datatype(self, datatype):  # type: (TimeSeries) -> None
-        self.timeseries_widget.reset_data()
+        # self.timeseries_widget.reset_data()
         self.timeseries_widget.add_datatype(datatype)
