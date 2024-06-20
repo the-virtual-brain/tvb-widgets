04cff19dc95be0fb87377cf89456a4336a5393de
unknown <priyayadav012004@gmail.com>
2024-06-20 17:02:20 +0530

    added options for conduction speed and time range

--- tvbwidgets/ui/spacetime_widget.py
+++ tvbwidgets/ui/spacetime_widget.py
@@ -7,7 +7,7 @@ from matplotlib.gridspec import GridSpec
 from IPython.display import display
 from tvb.datatypes.connectivity import Connectivity
 from tvbwidgets.ui.base_widget import TVBWidget
-from ipywidgets import Tab, Output
+from ipywidgets import Tab, Output, HBox, BoundedFloatText, FloatText, Layout
 
 
 class SpaceTimeVisualizerWidget(TVBWidget):
@@ -17,29 +17,28 @@ class SpaceTimeVisualizerWidget(TVBWidget):
         self.view_width = width
         self.view_height = height
         self.connectivity = connectivity
-        self.from_time = 0.00
-        self.to_time = 153.47
-        self.conduction_speed = 1.0
+        self.add_options()
+        self.conduction_speed = self.options.children[0].value
+        self.from_time = self.options.children[1].value
+        self.to_time =  self.options.children[2].value
         self.num_slices = 6
         self._prepare_tab()
-
 
     def _prepare_tab(self):
         self.tab = Tab()
         self._prepare_scene()
 
-        graphs_pythreejs = Output()
-        with graphs_pythreejs:
+        self.graphs_pythreejs = Output()
+        with self.graphs_pythreejs:
             display(self.renderer)
 
-        graphs_matplotlib = Output()
-        fig = self.create_matplotlib_graphs()
-        with graphs_matplotlib:
-            display(fig)
+        self.graphs_matplotlib = Output()
+        self.create_matplotlib_graphs()
+        with self.graphs_matplotlib:
+            display(self.fig)
 
-        self.tab.children = [graphs_pythreejs, graphs_matplotlib]
+        self.tab.children = [self.graphs_pythreejs, self.graphs_matplotlib]
         
-
     def _prepare_scene(self):
         self.camera = p3.PerspectiveCamera(position=[23, 7, -6], aspect=2)
         self.light = p3.AmbientLight()
@@ -52,12 +51,13 @@ class SpaceTimeVisualizerWidget(TVBWidget):
         self.renderer = p3.Renderer(camera=self.camera, scene=self.scene, controls=[self.controls],
                                     width=self.view_width, height=self.view_height)
         
-        
     def _prepare_slices(self):
         total_slices = self.num_slices+1
+        self.graph_slices = []
         for i in range(total_slices):
             graph_slice = self._create_graph_slice(i)
             self.scene.add(graph_slice)
+            self.graph_slices.append(graph_slice)
 
     def _create_graph_slice(self, i):
         z_coordinate = -i * 2.5 + 15
@@ -100,32 +100,79 @@ class SpaceTimeVisualizerWidget(TVBWidget):
         if i == 0:
             connectivity = self.connectivity.weights
         else:
-            slice_range = (self.to_time * i) / self.num_slices
-            prev_slice_range = (self.to_time * (i - 1)) / self.num_slices
+            slice_range = ((self.to_time-self.from_time) * i) / self.num_slices
+            prev_slice_range =  ((self.to_time-self.from_time) * (i - 1)) / self.num_slices
             time_delay = self.connectivity.tract_lengths * self.conduction_speed
-            mask = (time_delay < slice_range) & (time_delay > prev_slice_range)
+            mask = (time_delay < slice_range) & (time_delay > prev_slice_range) 
             connectivity = np.where(mask, self.connectivity.weights, 0)
         return connectivity
     
     def create_matplotlib_graphs(self):
-        fig = plt.figure(figsize=(14, 10))
-        gs = GridSpec(3, 4, figure = fig)
+        self.fig = plt.figure(figsize=(14, 10))
+        self.ims = []
+        gs = GridSpec(3, 4, figure = self.fig)
         
         for i in range(self.num_slices + 1):
             position = gs[int(i/3), int(i%3)]
-            ax = fig.add_subplot(position)
+            ax = self.fig.add_subplot(position)
             connectivity = self._prepare_connectivity(i)
             colors = self._generate_colors(connectivity)
-            im = ax.imshow(colors)
+            ax.imshow(colors)
             ax.set_xticks([]) 
-            ax.set_yticks([]) 
+            ax.set_yticks([])
+            self.ims.append(ax)
+         
+        self.fig.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0.01, hspace=0.01)
+        plt.close(self.fig)     
 
-        fig.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0.01, hspace=0.01)
-        plt.close(fig) 
-        return fig     
+    def add_options(self):
+        self.options = HBox(layout = Layout(width = '500px'))
+        self.option_conduction_speed = FloatText(
+                value=1.0,
+                step=0.1,
+                layout=Layout(width='150px'), 
+                style={'description_width': 'initial'},
+                description='Conduction Speed:',
+                disabled=False
+                )
+        self.option_conduction_speed.observe(self.on_change,names = "value")
+        self.option_from_time = BoundedFloatText(
+                    value=0.0,
+                    min=0.0,
+                    max=153.49,
+                    step=0.1,
+                    layout=Layout(width='150px'), 
+                    description='from[ms]:',
+                    disabled=False
+                )
+        self.option_from_time.observe(self.on_change,names = "value")
+        self.option_to_time = BoundedFloatText(
+                    value= 153.49,
+                    min=0.0,
+                    max=153.49,
+                    step=0.1,
+                    layout=Layout(width='150px'), 
+                    description='to[ms]:',
+                    disabled=False
+                )
+        self.option_to_time.observe(self.on_change,names = "value")
+        self.options.children = [self.option_conduction_speed, self.option_from_time, self.option_to_time]
+
+    def on_change(self,change):
+        self.graphs_matplotlib.clear_output()
+        self.conduction_speed = self.options.children[0].value
+        self.from_time = self.options.children[1].value
+        self.to_time =  self.options.children[2].value
+
+        for i in range(len(self.graph_slices)):
+            connectivity = self._prepare_connectivity(i)
+            data = self._generate_colors(connectivity)
+            self.graph_slices[i].material.map.data = data
+            self.ims[i].imshow(data)
+
+        with self.graphs_matplotlib:
+            display(self.fig)
 
     def display(self):
+        display(self.options)
         display(self.tab)
-
-        
-
