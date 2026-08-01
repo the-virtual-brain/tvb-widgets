29239098e39a4d3d40c5e31ad7ce43afcc98b05c
brainy-joseph <86651761+brainy-joseph@users.noreply.github.com>
2026-08-02 00:27:58 +0530

    Set notebook execution timeout to 300 seconds
    Increase timeout for notebook execution from None to 300 seconds.

--- dev/notebook_runner.py
+++ dev/notebook_runner.py
@@ -17,7 +17,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 def execute_notebook(in_path, notebook):
     with open(os.path.join(in_path, notebook), encoding='utf-8') as f:
         nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=None)
+        ep = ExecutePreprocessor(timeout=300)
         ep.preprocess(nb)
 
     # with open(os.path.join(out_path, notebook), 'w+', encoding='utf-8') as f: # for debug only
